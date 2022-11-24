167e5f2bc9aef3805d36a22329c239757eb1fd44
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-24 14:35:10 -0400

    Created Route pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div></div>
-  )
+        <main>
+            <Head>
+                <title>btix.blog | Home</title>
+            </Head>     
+            <h2></h2>
+        </main>
+    )
 }
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Posts(){
+    return(
+        <main>
+        <Head>
+            <title>btix.blog | posts</title>
+        </Head>     
+        <h2>Posts</h2>
+    </main>
+    );
+}
