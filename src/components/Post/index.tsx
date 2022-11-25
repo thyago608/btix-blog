4cf40e60d89e1c5c9aa0b6f5443aaca1f30e7271
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-24 21:13:14 -0400

    Styled Post Component and Post Page

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,5 +1,19 @@
 import Link from "next/link";
+import styles from "./styles.module.scss";
 
 export function Post() {
-  return <Link href="/"></Link>;
+  return (
+    <Link href="/" className={styles.post}>
+      <strong className={styles.title}>
+        sunt aut facere repellat provident occaecati
+        excepturi optio reprehenderit
+      </strong>
+      <p className={styles.content}>
+        quia et suscipit\nsuscipit recusandae consequuntur
+        expedita et cum\nreprehenderit molestiae ut ut quas
+        totam\nnostrum rerum est autem sunt rem eveniet
+        architecto
+      </p>
+    </Link>
+  );
 }
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,26 @@
 import { Post } from "components/Post";
 import Head from "next/head";
+import styles from "./styles.module.scss";
 
 export default function Posts() {
   return (
-    <main>
+    <main className={styles.container}>
       <Head>
         <title>btix.blog | posts</title>
       </Head>
-      <Post />
+      <section className={styles.postList}>
+        <header>
+          <h2 className={styles.heading}>Todos os posts</h2>
+        </header>
+        <div className={styles.list}>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </section>
     </main>
   );
 }
