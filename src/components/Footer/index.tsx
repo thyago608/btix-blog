d90e1978a818a2b1da8daff0d359c10ba8189561
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-24 21:19:41 -0400

    Created and Styled Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Btix Blog © Todos os direitos reservados</p>
+    </footer>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from "next/app";
-import { Header } from "components/Header"
+import { Header } from "components/Header";
 import "styles/global.scss";
+import { Footer } from "components/Footer";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
