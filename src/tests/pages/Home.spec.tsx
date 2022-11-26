4b66c4572bdedfea4cab0836a49cc51bed162d40
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-26 16:37:07 -0400

    Home page test created

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,35 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "../../pages";
+
+describe("Component Home", () => {
+  it("should render correctly", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /confira as últimas novidades da btix/i,
+    });
+
+    const textInformation = screen.getByText(
+      /nosso blog é atualizado toda semana/i
+    );
+
+    const postsLink = screen.getByRole("link", {
+      name: /ver posts/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+    expect(textInformation).toBeInTheDocument();
+    expect(postsLink).toBeInTheDocument();
+  });
+
+  it("should be go to posts page", () => {
+    render(<Home />);
+
+    const postsLink = screen.getByRole("link", {
+      name: /ver posts/i,
+    });
+
+    expect(postsLink).toHaveAttribute("href", "/posts");
+  });
+});
