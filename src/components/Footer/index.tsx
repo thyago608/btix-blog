c2c8932513e584a478523830f1fa452382b7e328
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-11-26 20:15:01 -0400

    Added Button Scroll Top in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,26 @@
 import styles from "./styles.module.scss";
+import { CaretUp } from "phosphor-react";
 
 export function Footer() {
+  function handleScrollUp() {
+    const htmlTagRoot = document.querySelector("html");
+
+    htmlTagRoot?.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <footer className={styles.footer}>
       <p>Btix Blog © Todos os direitos reservados</p>
+      <button
+        type="button"
+        onClick={handleScrollUp}
+        className={styles.buttonScrollUp}>
+        <CaretUp size={25} />
+      </button>
     </footer>
   );
 }
